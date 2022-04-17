2c43b17d954716c3cd553a0801bdcf1fbb546682
Mark-Kohrman <mark.kohrman@gmail.com>
2022-04-16 20:43:46 -0500

    refactor recursion fibo

--- tests/recursion_tests/fibonacci.test.js
+++ tests/recursion_tests/fibonacci.test.js
@@ -1,18 +1,14 @@
-const fibo = require('../../recursion/fibonacci');
+const fib = require('../../recursion/fibonacci');
 
 test('return the the 6th fibonacci number 8', () => {
-  expect(fibo(6)).toBe(8);
+  expect(fib(6)).toBe(8);
 });
 
 test('return the 10th fibonacci number 55', () => {
-  expect(fibo(10)).toBe(55);
-});
-
-test('return return 0 if n is 0', () => {
-  expect(fibo(0)).toBe(0);
+  expect(fib(10)).toBe(55);
 });
 
 test('return 1 if n is 2', () => {
-  expect(fibo(2)).toBe(1);
+  expect(fib(2)).toBe(1);
 });
 
